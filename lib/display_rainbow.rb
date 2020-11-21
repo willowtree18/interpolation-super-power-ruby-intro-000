9cc92390cf323dc4f3c20e0c311e59a4cc66d4cd
Emily Willow <emily.shapiera@gmail.com>
2020-11-21 18:37:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,4 @@ colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow (colours)
   puts "R: #{colour[0]}, O: #{colour[1]}, Y: #{colour[2]}, G: #{colour[3]}, B: #{colour[4]}, I: #{colour[5]}, V: #{colour[6]}"
+end
